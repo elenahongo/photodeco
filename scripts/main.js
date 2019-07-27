56946ab0790a064e0ed6c86cc6acd0063a4e5c40
Elena Hernandez Vazquez <selenelunaw@hotmail.com>
2019-07-27 01:18:43 -0500

    algo le paso al boton flotante

--- scripts/main.js
+++ scripts/main.js
@@ -66,8 +66,26 @@ if ($('.grid').length > 0) {
 }
 
 if ($('.fotos').length > 0) {
-  $('.navbar').removeClass('fixed-top');
+  //$('.navbar').removeClass('fixed-top');
   $('#black-background').css("background-color","black");
+
+  // When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar-secondary");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
 }
 
 });
